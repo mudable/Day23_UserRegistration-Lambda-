bd5bf86fe0002a919ddacadaf770301e8ece651b
mudable <mudableshilpa@gmail.com>
2021-11-18 12:54:11 +0530

    Added : UC3 to check the valid mail

--- src/main/java/com/bridgelabz/lambda/UserRegistration.java
+++ src/main/java/com/bridgelabz/lambda/UserRegistration.java
@@ -27,6 +27,12 @@ public class UserRegistration {
 			return userReg.Name(lastName);
 		};
 		System.out.println(LastName.validate());
+		Validation Email = () -> {
+			System.out.println("Enter Email ID :: ");
+			String EmailId = sc.next();
+			return userReg.Email(EmailId);
+		};
+		System.out.println(Email.validate());
 	}
 
 	private boolean Name(String firstName) {
@@ -40,6 +46,13 @@ public class UserRegistration {
 		String regex = "^[A-Z]{1}[a-z]{3,}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(lastName);
+		return matcher.matches();
+	}
+
+	private boolean Email(String email) {
+		String regex ="^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
 		return matcher.matches();
 	}
 }
